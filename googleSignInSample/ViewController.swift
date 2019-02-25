663d987917cad980e5147cf897ffe638804a02d8
Rajmohan Govindaswamy <rajmohan@cognitidigital.com>
2019-02-25 11:01:48 +0530

    Initial Commit

---
+++ googleSignInSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  googleSignInSample
+//
+//  Created by Cogniti Digital on 25/02/19.
+//  Copyright © 2019 Cogniti Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
